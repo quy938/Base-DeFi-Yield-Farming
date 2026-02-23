190a8e43e239c36c7ee7e62f6d6327ed8d2cd220
Tony Plutony <cutefomlower1g0@hotmail.com>
2026-02-23 20:54:02 +0200

    Update hardhat.config.js

--- scripts/hardhat.config.js
+++ scripts/hardhat.config.js
@@ -49,7 +49,7 @@ module.exports = {
     apiKey: process.env.ETHERSCAN_API_KEY || ""
   },
 
-  // Some projects may have different folder structures.
+
   // Keep default unless you have a custom /contracts path already.
   paths: {
     sources: "./contracts",
